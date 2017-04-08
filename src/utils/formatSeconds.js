c86da8ea091f52de0d2c874dd7b0ea517ab2c559
Sabeur Thabti <thabti.sabeur@gmail.com>
2017-04-08 05:16:39 +0100

    clean up

--- src/utils/formatSeconds.js
+++ src/utils/formatSeconds.js
@@ -2,10 +2,5 @@ export default (seconds) => {
   const date = new Date(null);
   date.setSeconds(seconds); // specify value for SECONDS here
   const time = date.toISOString().substr(11, 8);
-  const splitTime = time.split(':');
-  // if (splitTime[0] === '00') {
-  //   splitTime.shift();
-  //   return splitTime.join(':');
-  // }
   return time;
 };
